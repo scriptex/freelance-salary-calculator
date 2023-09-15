93467a1d1dd8cbed4ae97b3b5917945c2c952ab6
Atanas Atanasov <hi@atanas.info>
2023-09-15 10:00:07 +0300

    Convert to PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const withPWA = require('next-pwa')({
+	dest: 'public'
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
